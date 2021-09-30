c13af0ff63a6238edabaa4c2a9855abb43c830ea
Hiran Oliveira <oliveirahiran1@gmail.com>
2021-09-30 14:56:25 -0300

    atividade finalizada

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,15 @@
+import { useDispatch } from "react-redux";
+import { addNumber, subNumber } from "../../store/modules/Results/actions";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <button onClick={() => dispatch(addNumber(1))}>Add +</button>
+      <button onClick={() => dispatch(subNumber(1))}>Add -</button>
+    </>
+  );
+};
+
+export default Counter;
---
+++ src/store/modules/Results/reducer.js
@@ -0,0 +1,17 @@
+import { ADD_NUMBER, SUB_NUMBER } from "./actionTypes";
+
+const resultReducer = (state = 0, action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_NUMBER:
+      return state + action.payload;
+
+    case SUB_NUMBER:
+      return state - action.payload;
+
+    default:
+      return state;
+  }
+};
+
+export default resultReducer;
